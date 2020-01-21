d415bb45c42f9d1f09b3cea45077f7593531d1e2
Jake Armstrong <Lainkai@users.noreply.github.com>
2020-01-21 10:29:24 -0500

    Added todos

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  //TODO Add SendableChooser
   private RobotContainer m_robotContainer;
 
   /**
@@ -31,6 +31,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    //Sendable chooser options here.
   }
 
   /**
@@ -65,6 +66,7 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void autonomousInit() {
+    //chooser.getSelected().schedule();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
