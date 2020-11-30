2b5d9e8b29f08e6f97d4a2600018044b46a15019
ProgrammingLaptop <43614946+team3487@users.noreply.github.com>
2020-11-30 11:26:25 -0500

    Final Changes
    Co-Authored-By: Jake Armstrong <lainkai@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import com.kauailabs.navx.frc.AHRS;
+
 
 public class DriveSubsystem extends SubsystemBase {
 
@@ -12,6 +15,9 @@ public class DriveSubsystem extends SubsystemBase {
     private SpeedController rightMotor = new Spark(Constants.DRIVE_RIGHT);
     private final DifferentialDrive drive = new DifferentialDrive(leftMotor, rightMotor);
 
+    public DriveSubsystem() {
+        
+    }
     public void setSpeed(double left, double right) {
         drive.tankDrive(left, right);
     }
